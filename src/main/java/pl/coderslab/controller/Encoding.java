49bd8268c412e647020ab61d65bc08235dbdae22
nzdunek <noela.zdunek@gmail.com>
2019-08-27 16:45:58 +0200

    metoda i wyświetlanie ilości planów i przepisów użytkownika, bez sesji logowania. Niedokończone wyświetlanie szczegółów planu

---
+++ src/main/java/pl/coderslab/controller/Encoding.java
@@ -0,0 +1,24 @@
+package pl.coderslab.controller;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class Encoding implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html");
+
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
